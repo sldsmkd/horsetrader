ba09a195abe1306b864fc6e9fcfb0e9ed4c2609a
Kris Holdich <kris@smkd.net>
2026-05-28 05:34:39 +0100

    Loose stuff

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from horsetrader.pipeline import Pipeline
 
 if __name__ == "__main__":
     Pipeline().run()
-    # print(json.dumps(Pipeline().metrics, indent=2))
+    print(json.dumps(Pipeline().metrics, indent=2))
